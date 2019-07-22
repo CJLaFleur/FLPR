070179a84f956fe5ac609a889c0670521fe9299a
CJ LaFleur <clafleur841@gmail.com>
2019-07-22 13:39:16 -0400

    Automated deleting profiles, removing and rea-adding computer from domain.

---
+++ PowerShell/dev/Prep-Computer.ps1
@@ -0,0 +1,20 @@
+﻿function Prep-Computer {
+
+    #$Creds = Get-Credential
+
+    $Profiles = Get-CimInstance -ClassName Win32_UserProfile #| Where {(!$_.Special)}
+
+    foreach ($i in $Profiles) {
+
+        echo $i
+        echo "-----------------------------------------------------------"
+
+    }
+    
+    #Remove-Computer -UnjoinDomainCredential $Creds -WorkgroupName Temp
+
+    #Start-Sleep -Seconds 5
+
+    #Add-Computer -Credential $Creds -DomainName firstlightpower.com
+
+}
---
+++ PowerShell/dev/Prep-Win7.ps1
@@ -0,0 +1,17 @@
+﻿function Prep-Win7 {
+
+    # MAKE SURE TO FORMAT YOUR CREDENTIALS AS FOLLOWS: "firstlightpower.com\cjlafleur"
+
+    $Creds = Get-Credential
+    
+    $Domain = "FIRSTLIGHTPOWER.COM"
+
+    Get-WmiObject Win32_UserProfile | Where {($_.LocalPath -NE "C:\Users\ITD" -AND !$_.Special -AND $_.Loaded -EQ $FALSE)} | Remove-WmiObject
+    
+    Remove-Computer -Credential $Creds -Force
+
+    Add-Computer -Credential $Creds -DomainName $Domain
+    
+    Restart-Computer
+
+}
