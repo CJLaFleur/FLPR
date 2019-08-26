5e1354e18a734db2cd7076b24170cc7085db70fa
CJ LaFleur <clafleur841@gmail.com>
2019-08-26 15:35:32 -0400

    Working on adding details for USMT

--- PowerShell/dev/Prep-Computer.ps1
+++ PowerShell/dev/Prep-Computer.ps1
@@ -5,29 +5,48 @@
     [cmdletbinding()]
     Param(
 
-        [Parameter(Mandatory = $False,
-        Position = 0,
+        [Parameter(Mandatory = $True,
         HelpMessage ="Enter the primary user on this device")]
-        [String] $User
+        [String] $User,
+
+        [Parameter(Mandatory = $True,
+        HelpMessage ="Enter the name of the computer to be copied from")]
+        [String] $OldComputer,
+
+        [Parameter(Mandatory = $True,
+        HelpMessage ="Enter the name of the computer to be copied to")]
+        [String] $NewComputer
 
         )
+
+    BEGIN {
 
     $Creds = Get-Credential
     
     $Domain = "FIRSTLIGHTPOWER.COM"
 
-    Get-CimInstance -ClassName Win32_UserProfile | Where {($_.LocalPath -NE "C:\Users\$User" -AND !$_.Special -AND $_.Loaded -EQ $False)} | Remove-CimInstance
+    }
+
+    PROCESS {
+
+    Get-WmiObject -ComputerName $OldComputer -ClassName Win32_UserProfile | Where {($_.LocalPath -NE "C:\Users\$User" -AND !$_.Special -AND $_.Loaded -EQ $False)} | Remove-WmiObject
+
+    Invoke-Command -ComputerName $OldComputer -Credential $Creds -ScriptBlock { \\burapp017\scanstate.exe \\datastore\$OldComputer /i:miguser.xml /i:migapp.xml /o /c }
     
     Remove-Computer -Credential $Creds -Force
 
-    .\scanstate.exe d:\chopp /i:miguser.xml /i:migapp.xml /o /c
+    Restart-Computer -Force
 
-    Add-Computer -Credential $Creds -DomainName $Domain
+    Add-Computer -ComputerName $NewComputer -Credential $Creds -DomainName $Domain
+
+    \\burapp017\loadstate.exe /i:migapp.xml /i:miguser.xml \\datastore\$OldComputer /all /progress:progress.log /l:load.log /c /lac
 
     $OU = Get-ADOrganizationalUnit -LDAPFilter "(name=Computers)"
 
-    Get-ADComputer -LDAPFilter "(name=$($env:COMPUTERNAME))" | Move-ADObject -TargetPath $OU.DistinguishedName
+    Get-ADComputer -LDAPFilter "(name=$($NewComputer))" | Move-ADObject -TargetPath $OU.DistinguishedName
     
     Restart-Computer
+
+    }
 
 }
