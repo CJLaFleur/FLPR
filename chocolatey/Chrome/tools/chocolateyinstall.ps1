ccf857b09b85a232c5be56130b2a53d4772ec10f
CJ LaFleur <clafleur841@gmail.com>
2019-08-28 13:48:37 -0400

    Chrome Chocolatey package

---
+++ chocolatey/Chrome/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'chrome'
+$fileType = 'msi'
+$fileLocation = "http://10.0.2.11/choco/Chrome/GoogleChromeEnterpriseBundle64/Installers/GoogleChromeStandaloneEnterprise64.msi"
+#$fileLocation = "http://10.0.2.11/packages/chrome/GoogleChromeStandaloneEnterprise64.msi"
+#$fileLocation = "http://10.0.2.11/choco/Chrome/GoogleChromeEnterpriseBundle64/Installers/GoogleChromeStandaloneEnterprise64.msi"
+$silentArgs = '/qn'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $fileLocation
